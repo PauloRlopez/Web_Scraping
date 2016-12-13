d17247d2abf42491bf34d307366fe960fefccb41
Paulo R Lopez <Lopez_Paulo@outlook.com>
2016-12-13 06:10:16 -0800

    Create TopRatedIndianMovies.py

---
+++ TopRatedIndianMovies.py
@@ -0,0 +1,26 @@
+
+#This script will find the top rated Indina movies movies 
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+driver = webdriver.PhantomJS(executable_path = r'/Users/Create/WebScrapping/phantomjs/bin/phantomjs')
+
+url = 'http://www.imdb.com/chart/top-indian-movies?ref_=nv_mv_250_in_7'
+
+driver.get(url)
+
+#print(driver.page_source)
+
+soup = BeautifulSoup(driver.page_source,'lxml')
+
+table = soup.find('table', class_ = 'chart full-width')
+
+#for a in table.find_all('a'):
+#	print(a.text)
+
+for td in table.find_all('td', class_ = 'titleColumn'):
+	print(td.text.strip().replace('\n','').replace('      ',''))
+
+
+
